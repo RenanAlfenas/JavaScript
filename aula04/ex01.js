7dde0b10d84b547e236c229256f3872f58d7e471
RenanAlfenas <alfenas57@gmail.com>
2025-07-06 12:33:15 -0300

    ex03

---
+++ aula04/ex01.js
@@ -0,0 +1,7 @@
+var vel = 40
+
+if (vel > 60) {
+    console.log(` a sua velocidade é de ${vel} km por hora. a velocidade máxima permitida é de 60km/h. VOCÊ FOI MULTADO!`)
+} else {
+    console.log(` sua velocidade é de ${vel} km/h você está dentro do limite de 60km/h. dirija com cuidado e use sempre o cinto de segurança`)
+}
