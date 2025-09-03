a544280ef391430b4175a76d4c03d77084fe4ef1
RenanAlfenas <alfenas57@gmail.com>
2025-09-02 23:20:21 -0300

    script contador

--- exercicioaula14/scripts/javascript.js
+++ exercicioaula14/scripts/javascript.js
@@ -4,34 +4,33 @@ function contar() {
     var ini = window.document.getElementById('textinicio')
     var fim = window.document.getElementById('textfim')
     var pass = window.document.getElementById('textpasso')
+    var res = window.document.getElementById('res')
 
-    var ininum =  Number( ini.value )
-    var fimnum = Number(fim.value)
-    var passo = Number(pass.value)
-    var resultado = window.document.getElementById('res')
-
-        if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0 ) {
-            window.alert('erro! faltam dados.')
-        }   else if (ininum < fimnum) {
-            for (var c = ininum ; c <= fimnum ; c = c + passo) {
-                resultado.innerText += ` ${c}`
-            }
-        }
+    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0) {
+        window.alert('erro! verifique se os valores foram todos preenchidos!')
+    } else {
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(pass.value)
         
-        if (ininum > fimnum ) {
-            for (var c = ininum; c >= fimnum; c = c - passo) {
-                resultado.innerText += ` ${c}`
-            }
+    
 
-        } else if (passo <= 0) {
-            window.alert('Passo inválido. considerarei passo = 1')
-            for (var c = ininum; c <= fimnum; c = c + passo) {
-                resultado.innerText += ` ${c}`
+    if (p <= 0) {
+        window.alert('erro! o valor do passo não é aceito. usaremos p = 1')
+        p = 1
+            for( var c = i ; c <= f ; c = c + 1) {
+                res.innerText += ` ${c}`
             }
+    }else if (i < f ) {
+            for(var c = i ; c <= f ; c = c + p) {
+                res.innerText += ` ${c}`
         }
-            
+    } else if ( i > f) {
+            for(var c = i ; c >= f ; c = c - p) {
+                res.innerText += ` ${c}`
+        }
+    }
 
-  
-
+    }
 }
 
